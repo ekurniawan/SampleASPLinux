e97c31546889072498a0284534282bf937b97a00
Erick Kurniawan <erick.kurniawan@gmail.com>
2019-06-17 21:01:13 +0700

    Add project files.

---
+++ ASPCoreWebAPI/Controllers/HelloController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASPCoreWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HelloController : ControllerBase
+    {
+        public IActionResult HelloWorld()
+        {
+            return Content("Hello ASP Linux !");
+        }
+    }
+}
